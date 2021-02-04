f97cf0424fe1d88bf5dfed5cdf85abaff6ab201c
Shubhanshu Arya <shubhanshuarya2020@gmail.com>
2021-02-04 16:17:24 +0530

    Buynow.html Url Created Successfully

--- frontend_app/urls.py
+++ frontend_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home),
     path('product-detail/', views.product_detail, name="product-detail"),
     path('cart/', views.add_to_cart, name='add-to-cart'),
+    path('buy/', views.buy_now, name='buy-now'),
 ]

650d5b30e2218a6147524229e17d18229a6156d6
Shubhanshu Arya <shubhanshuarya2020@gmail.com>
2021-02-04 15:17:39 +0530

    Integating frontend_app to Project

--- shoppingwebsite/settings.py
+++ shoppingwebsite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Adding Own Applications
+    "frontend_app",
 ]
 
 MIDDLEWARE = [

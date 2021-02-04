6a1f0f56843d2b3ad7c95cb6a26e37ea18bd9a6c
Shubhanshu Arya <shubhanshuarya2020@gmail.com>
2021-02-04 16:21:57 +0530

    profile.html Url Created Successfully

--- frontend_app/urls.py
+++ frontend_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('product-detail/', views.product_detail, name="product-detail"),
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
+    path('profile/', views.profile, name='profile'),
+
 ]

48bdebc86b32cfb7a9d4a1b0d6d4cdff9f056162
Shubhanshu Arya <45733409+Shubhanshuarya@users.noreply.github.com>
2021-06-28 16:08:53 +0530

    Update settings.py

--- shoppingwebsite/settings.py
+++ shoppingwebsite/settings.py
@@ -167,5 +167,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shubhanshuarya2019@gmail.com'
-EMAIL_HOST_PASSWORD = "JAIMATADI2019s"
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = "enter_password"
